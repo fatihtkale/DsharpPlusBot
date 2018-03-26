4cbae65b5af909cda5e259b2dca5764908cf7c73
itzyzex <iosbroker2720@gmail.com>
2018-03-26 21:23:17 +0200

    Revert "Added new spank command :O"
    This reverts commit 30b08c2d4c615face9497975a291fd957f30960b.

--- src/Program.cs
+++ src/Program.cs
@@ -7,36 +7,21 @@ using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using System.Drawing;
-using ImageMagick;
-using ImageMagick.Configuration;
 using System.Linq;
 namespace DiscordBottest
 {
     class Program
     {
-        static DiscordClient discord;
-        static CommandsNextModule commands;
-        static Curse curses = new Curse();
-        static Scam scam = new Scam();
-        
-        static void Main(string[] args)
-        {
-            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
-        }
-        static async Task MainAsync(string[] args)
-        {
-            discord = new DiscordClient(new DiscordConfiguration
+            static DiscordClient discord;
+            static Curse curses = new Curse();
+            static CommandsNextModule commands;
+            static void Main(string[] args)
             {
-                AutoReconnect = true,
-                Token = "TOKEN",
-                TokenType = TokenType.Bot
-            });
-            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+                MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+            static async Task MainAsync(string[] args)
             {
-                StringPrefix = "!"
-            });
-            discord.MessageCreated += async e =>
-            {
+<<<<<<< HEAD
                 if (Scam.censoredWords.Any(x => e.Message.Content.ToLower().Contains(x))){
                     var scamlink = new DiscordEmbedBuilder()
                     .WithColor(new DiscordColor("#f03434"))
@@ -73,6 +58,12 @@ namespace DiscordBottest
                 {
                     AutoReconnect = true,
                     Token = "NDIyNDI3NjU3MjQyODA0MjI0.DYnTBw",
+=======
+                discord = new DiscordClient(new DiscordConfiguration
+                {
+                    AutoReconnect = true,
+                    Token = "NDIyNDI3NjU3MjQyODA0MjI0.DYnTBw.TrjvtbAmyY-xVnnpCx5Q9dAi0lA",
+>>>>>>> parent of 30b08c2... Added new spank command :O
                     TokenType = TokenType.Bot
                 });
                 commands = discord.UseCommandsNext(new CommandsNextConfiguration
@@ -100,44 +91,35 @@ namespace DiscordBottest
             }
             public class Commands
             {
-                [Command("spank")]
-                [Description("Changes Nickname")]
-                public async Task spank(CommandContext ctx,[Description("!spank <name>")] DiscordMember user){
-                    using (MagickImage image = new MagickImage("spanking.jpg"))
-                    {
-                        using (MagickImage watermark = new MagickImage(ctx.Message.Author.AvatarUrl))
-                        {
-                            // Draw the watermark in the bottom right corner
-                            image.Composite(watermark, Gravity.North, CompositeOperator.Over);
-                            var water = new MagickImage(user.AvatarUrl);
-                            water.Resize(200, 200);
-                            image.Composite(water, 130, 400, CompositeOperator.Over);
-                        }
-                        image.Write("spank.jpg");
-                    }
-                    await ctx.RespondWithFileAsync("spank.jpg", $"You spanked {user.Username}#{user.Discriminator} 🍑👏");
-                }
- 
                 [Command("changenick")]
-                [Description("Changes Nickname")]
-                public async Task Changenick(CommandContext ctx,[Description("!changenick <name>")] [RemainingText] string name){
+                public async Task Changenick(CommandContext ctx,[RemainingText]  string name){
                     await ctx.RespondAsync($"Changed Nickname of, {ctx.User.Mention}, to " + name);
                     await ctx.Member.ModifyAsync(nickname: name);
                 }
 
                 [Command("hi")]
-                [Description("Says hi to specified user!")]
                 public async Task Hi(CommandContext ctx)
                 {
                     await ctx.RespondAsync($"👋 Hello!, {ctx.User.Mention}!");
                 }
-                
+
                 [Command("random")]
-                [Description("Rolls a random number!")]
                 public async Task Random(CommandContext ctx, int min, int max)
                 {
                     var rnd = new Random();
                     await ctx.RespondAsync($"🎲 Your random number is: {rnd.Next(min, max)}!");
+                }
+               [Command("commands")]
+                public async Task comds(CommandContext ctx)
+                {
+                    var helpembed = new DiscordEmbedBuilder()
+                    .WithDescription("this shows all of the commands")
+                    .WithColor(new DiscordColor("#f03434"))
+                    .WithAuthor("Commands")
+                    .AddField("Changenick", "!changenick - Changes username of a person. Usage: !changenick <name>", false)
+                    .AddField("Random", "!random - Roll a number. Usage: !random <minnumber> <maxnumber>", false)
+                    .AddField("Hi", "!hi - bot greets you!", false);
+                    await ctx.RespondAsync(null, false, helpembed);
                 }
             }
     }
